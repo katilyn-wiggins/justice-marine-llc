d2be8e7b1cd73261c5a1d4e269d519e04f3c437d
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-08-03 10:56:59 -0400

    changed navy to blue of logo

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -8,7 +8,6 @@ export default class Hamburger extends React.Component {
   }
 
   render () {
-    // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
     return (
       <Menu>
         <a id="home" className="menu-item" href="/">Home</a>
