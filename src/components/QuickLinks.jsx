f3a27ca8da399c13f97c0d5f489e98948364c89f
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-08-05 14:11:47 -0400

    fixed footer

--- src/components/QuickLinks.jsx
+++ src/components/QuickLinks.jsx
@@ -5,15 +5,9 @@ import marin from '../images/marin-logo.png'
 const QuickLinks = () => (
   <div className="quick-links-container">
   <div className="quick-links-1"> 
-    <a href="#contact">Contact Us</a>
-    <a href="#about">About Us</a>
-    <a href="#mission">Mission</a>
-  </div>
-
-  <div className="quick-links-1"> 
-    <a href="#logo">FAQ</a>
-    <a href="#logo">Blog</a>
-    <a href="#logo">Resources</a>
+    <a href="/contact">Contact Us</a>
+    <a href="/about">About Us</a>
+    <a href="/equipment">Eqiupment</a>
   </div>
 
   <div className="quick-links-1"> 
